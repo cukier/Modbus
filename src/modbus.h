0fd81bc19d494097a7022b872cf333dd752df492
cuki2 <mauricio@krebs.com.br>
2016-04-07 15:46:12 -0300

    Em construcao

--- src/modbus.h
+++ src/modbus.h
@@ -8,6 +8,8 @@
 #ifndef MODBUS_H_
 #define MODBUS_H_
 
+#include <stdint.h>
+
 #define READ_COILS					0x01
 #define READ_DISCRETE_INPUTS		0x02
 #define READ_HOLDING_REGISTERS		0x03
@@ -22,24 +24,49 @@
 #define REQUEST_SIZE				8
 #define BUFFER_S					256
 
-extern const unsigned short wCRCTable[];
-extern unsigned char makeByte(unsigned short word, unsigned char index);
-extern unsigned short makeWord(unsigned char hiByte, unsigned char loByte);
-extern unsigned short troca(unsigned short in);
-extern unsigned short CRC16(unsigned char *nData, unsigned short wLength);
-extern unsigned char *make_request(unsigned char dev_addr, unsigned short from,
-		unsigned short to, unsigned char type);
-extern unsigned char *make_read_request(unsigned char dev_addr,
-		unsigned short from, unsigned short nr);
-extern unsigned char *make_write_request(unsigned char dev_addr,
-		unsigned short reg_addr, unsigned short reg_value);
-extern unsigned char *make_discrete_inputs_request(unsigned char dev_addr,
-		unsigned short reg_addr, unsigned short reg_value);
-extern unsigned char *make_read_coils_request(unsigned char dev_addr,
-		unsigned short reg_addr, unsigned short reg_value);
-extern int get_device(int fd, unsigned char dev_addr, unsigned short reg_addr,
-		unsigned short reg_data, unsigned char *device_memorie);
-extern unsigned char *fromFloat(float in);
-extern float toFloat(unsigned char *in);
+struct resp_str {
+	uint8_t addr;
+	uint8_t cmd;
+	uint8_t resp_size;
+	uint8_t *resp;
+	uint16_t crc;
+};
+
+struct req_str {
+	uint8_t addr;
+	uint8_t cmd;
+	uint16_t from;
+	uint16_t to;
+	uint16_t crc;
+};
+
+typedef union req_un {
+	struct req_str structure;
+	uint8_t string[sizeof(struct req_str)];
+} req_un_type;
+
+union {
+	float a;
+	uint8_t b[sizeof(float)];
+} un;
+
+extern const uint16_t wCRCTable[];
+extern uint8_t make_byte(uint16_t word, uint8_t index);
+extern uint16_t make_word(uint8_t hiByte, uint8_t loByte);
+extern uint16_t swap16(uint16_t in);
+extern uint16_t CRC16(uint8_t *nData, uint16_t wLength);
+extern uint8_t *make_request(uint8_t dev_addr, uint16_t from, uint16_t to,
+		uint8_t type);
+extern uint8_t *make_read_request(uint8_t dev_addr, uint16_t from, uint16_t nr);
+extern uint8_t *make_write_request(uint8_t dev_addr, uint16_t reg_addr,
+		uint16_t reg_value);
+extern uint8_t *make_discrete_inputs_request(uint8_t dev_addr,
+		uint16_t reg_addr, uint16_t reg_value);
+extern uint8_t *make_read_coils_request(uint8_t dev_addr, uint16_t reg_addr,
+		uint16_t reg_value);
+extern int get_device(int fd, uint8_t dev_addr, uint16_t reg_addr,
+		uint16_t reg_data, uint8_t *device_memorie);
+extern uint8_t *fromFloat(float in);
+extern float toFloat(uint8_t *in);
 
 #endif /* MODBUS_H_ */
