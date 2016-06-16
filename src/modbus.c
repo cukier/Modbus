103892fdd4858699c53bdd8a0065d63c3239742c
cuki2 <mauricio@krebs.com.br>
2016-06-16 15:45:57 -0300

    write multiple registers
    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- src/modbus.c
+++ src/modbus.c
@@ -1,70 +1,283 @@
 /*
  * modbus.c
  *
- *  Created on: 03/12/2014
+ *  Created on: 20/08/2015
  *      Author: cuki
+ *
+ *
+ *
  */
 
 #include "modbus.h"
-#include "serial.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-const uint16_t wCRCTable[] = { 0X0000, 0XC0C1, 0XC181, 0X0140, 0XC301, 0X03C0,
-		0X0280, 0XC241, 0XC601, 0X06C0, 0X0780, 0XC741, 0X0500, 0XC5C1, 0XC481,
-		0X0440, 0XCC01, 0X0CC0, 0X0D80, 0XCD41, 0X0F00, 0XCFC1, 0XCE81, 0X0E40,
-		0X0A00, 0XCAC1, 0XCB81, 0X0B40, 0XC901, 0X09C0, 0X0880, 0XC841, 0XD801,
-		0X18C0, 0X1980, 0XD941, 0X1B00, 0XDBC1, 0XDA81, 0X1A40, 0X1E00, 0XDEC1,
-		0XDF81, 0X1F40, 0XDD01, 0X1DC0, 0X1C80, 0XDC41, 0X1400, 0XD4C1, 0XD581,
-		0X1540, 0XD701, 0X17C0, 0X1680, 0XD641, 0XD201, 0X12C0, 0X1380, 0XD341,
-		0X1100, 0XD1C1, 0XD081, 0X1040, 0XF001, 0X30C0, 0X3180, 0XF141, 0X3300,
-		0XF3C1, 0XF281, 0X3240, 0X3600, 0XF6C1, 0XF781, 0X3740, 0XF501, 0X35C0,
-		0X3480, 0XF441, 0X3C00, 0XFCC1, 0XFD81, 0X3D40, 0XFF01, 0X3FC0, 0X3E80,
-		0XFE41, 0XFA01, 0X3AC0, 0X3B80, 0XFB41, 0X3900, 0XF9C1, 0XF881, 0X3840,
-		0X2800, 0XE8C1, 0XE981, 0X2940, 0XEB01, 0X2BC0, 0X2A80, 0XEA41, 0XEE01,
-		0X2EC0, 0X2F80, 0XEF41, 0X2D00, 0XEDC1, 0XEC81, 0X2C40, 0XE401, 0X24C0,
-		0X2580, 0XE541, 0X2700, 0XE7C1, 0XE681, 0X2640, 0X2200, 0XE2C1, 0XE381,
-		0X2340, 0XE101, 0X21C0, 0X2080, 0XE041, 0XA001, 0X60C0, 0X6180, 0XA141,
-		0X6300, 0XA3C1, 0XA281, 0X6240, 0X6600, 0XA6C1, 0XA781, 0X6740, 0XA501,
-		0X65C0, 0X6480, 0XA441, 0X6C00, 0XACC1, 0XAD81, 0X6D40, 0XAF01, 0X6FC0,
-		0X6E80, 0XAE41, 0XAA01, 0X6AC0, 0X6B80, 0XAB41, 0X6900, 0XA9C1, 0XA881,
-		0X6840, 0X7800, 0XB8C1, 0XB981, 0X7940, 0XBB01, 0X7BC0, 0X7A80, 0XBA41,
-		0XBE01, 0X7EC0, 0X7F80, 0XBF41, 0X7D00, 0XBDC1, 0XBC81, 0X7C40, 0XB401,
-		0X74C0, 0X7580, 0XB541, 0X7700, 0XB7C1, 0XB681, 0X7640, 0X7200, 0XB2C1,
-		0XB381, 0X7340, 0XB101, 0X71C0, 0X7080, 0XB041, 0X5000, 0X90C1, 0X9181,
-		0X5140, 0X9301, 0X53C0, 0X5280, 0X9241, 0X9601, 0X56C0, 0X5780, 0X9741,
-		0X5500, 0X95C1, 0X9481, 0X5440, 0X9C01, 0X5CC0, 0X5D80, 0X9D41, 0X5F00,
-		0X9FC1, 0X9E81, 0X5E40, 0X5A00, 0X9AC1, 0X9B81, 0X5B40, 0X9901, 0X59C0,
-		0X5880, 0X9841, 0X8801, 0X48C0, 0X4980, 0X8941, 0X4B00, 0X8BC1, 0X8A81,
-		0X4A40, 0X4E00, 0X8EC1, 0X8F81, 0X4F40, 0X8D01, 0X4DC0, 0X4C80, 0X8C41,
-		0X4400, 0X84C1, 0X8581, 0X4540, 0X8701, 0X47C0, 0X4680, 0X8641, 0X8201,
-		0X42C0, 0X4380, 0X8341, 0X4100, 0X81C1, 0X8081, 0X4040 };
+long swap_w(long word) {
+	long aux;
 
-uint8_t make_byte(uint16_t word, uint8_t index) {
-	uint8_t ret = 0;
-	uint16_t mask = 0xFF << (index * 8);
+	aux = (word & 0xFF00) >> 8;
+	aux |= (word & 0x00FF) << 8;
 
-	ret = (mask & word) >> (index * 8);
-
-	return ret;
+	return aux;
 }
 
-uint16_t make_word(uint8_t hiByte, uint8_t loByte) {
-	uint16_t ret = 0;
+#ifdef USE_UART1
+#use rs232(uart1, baud=UART1_BAUD_RATE, stream=sl1)
+#INT_RDA
+void isr_rda() {
+	clear_interrupt(INT_RDA);
+	uc_set_timer_rda();
+	uc_setup_timer_rda();
+	buffer_rda[index_rda++] = fgetc(sl1);
+	if (index_rda >= RDA_BUFFER_SIZE)
+	index_rda = 0;
+}
+#if (SL1_TYPE == SL_MASTER_SLAVE)
+#INT_TIMER0
+void isr_rda_timer() {
+	clear_interrupt(RDA_INTERRUPT);
+	index_rda = 0;
+	uc_set_timer_rda();
+	uc_turn_off_timer_rda();
+	slave_respond = TRUE;
+}
+#endif
+#endif
 
-	ret = (hiByte & 0xFF) << 8;
-	ret |= loByte & 0xFF;
+#ifdef USE_UART2
+#use rs232(uart2, baud=UART2_BAUD_RATE, stream=sl2)
+#INT_RDA2
+void isr_rda2() {
+	clear_interrupt(INT_RDA2);
+	uc_set_timer_rda2();
+	uc_setup_timer_rda2();
+	buffer_rda2[index_rda2++] = fgetc(sl2);
+	if (index_rda2 >= RDA2_BUFFER_SIZE)
+	index_rda2 = 0;
+}
+#if (SL2_TYPE == SL_MASTER_SLAVE)
+#INT_TIMER1
+void isr_rda2_timer() {
+	clear_interrupt(RDA2_INTERRUPT);
+	uc_set_timer_rda2();
+	uc_turn_off_timer_rda2();
+	slave_respond = TRUE;
+}
+#endif
+#endif
 
-	return ret;
+modbus_exception_t slave_response(door_t door) {
+	long cont, crc, register_value, register_address, b_count;
+	int *resp, *buff;
+	size_t size;
+	modbus_fields_t field;
+
+	size = 0;
+	buff = (door == SL1_DOOR) ? buffer_rda : buffer_rda2;
+	register_value = make16(buff[REGISTER_VALUE_H], buff[REGISTER_VALUE_L]);
+	register_address = make16(buff[REGISTER_ADDRESS_H],
+			buff[REGISTER_ADDRESS_L]);
+	b_count = buff[BYTE_COUNT];
+
+	if (my_address == buff[ADDRESS]) {
+		switch (buff[FUNCTION]) {
+		case READ_HOLDING_REGISTERS_COMMAND:
+
+			if (register_value == 0 || register_value > 0x007D) {
+				size = 5;
+				resp = NULL;
+				resp = (int *) malloc(size);
+
+				if (resp == NULL) {
+					free(resp);
+					return OUT_OF_MEMORY_EXCEPTION;
+				}
+
+				resp[0] = my_address;
+				resp[1] = 0x83;
+				resp[2] = 0x03;
+				crc = CRC16(resp, 3);
+			} else if (register_value + register_address >= UINT16_MAX) {
+				size = 5;
+				resp = NULL;
+				resp = (int *) malloc(size);
+
+				if (resp == NULL) {
+					free(resp);
+					return OUT_OF_MEMORY_EXCEPTION;
+				}
+
+				resp[0] = my_address;
+				resp[1] = 0x83;
+				resp[2] = 0x02;
+				crc = CRC16(resp, 3);
+			} else {
+				size = (size_t)((register_value * 2) + 5);
+				resp = NULL;
+				resp = (int *) malloc(size);
+
+				if (resp == NULL) {
+					free(resp);
+					return OUT_OF_MEMORY_EXCEPTION;
+				}
+
+				resp[0] = my_address;
+				resp[1] = READ_HOLDING_REGISTERS_COMMAND;
+				resp[2] = (int) register_value * 2;
+				for (cont = 0; cont < register_value; ++cont) {
+					resp[(2 * cont) + 3] = read_eeprom(
+							register_address + (2 * cont));
+					resp[(2 * cont) + 4] = read_eeprom(
+							register_address + ((2 * cont) + 1));
+				}
+				crc = CRC16(resp, (register_value * 2) + 3);
+			}
+
+			resp[size - 2] = make8(crc, 1);
+			resp[size - 1] = make8(crc, 0);
+			break;
+
+		case WRITE_SINGLE_REGISTER_COMMAND:
+
+			size = (size_t)(8);
+			resp = NULL;
+			resp = (int *) malloc(size);
+
+			if (resp == NULL) {
+				free(resp);
+				return OUT_OF_MEMORY_EXCEPTION;
+			}
+
+			write_eeprom(register_address * 2, make8(register_value, 1));
+			write_eeprom((register_address * 2) + 1, make8(register_value, 0));
+
+			resp[0] = my_address;
+			resp[1] = WRITE_SINGLE_REGISTER_COMMAND;
+			resp[2] = (int) make8(register_address, 1);
+			resp[3] = (int) make8(register_address, 0);
+			resp[4] = (int) make8(register_value, 1);
+			resp[5] = (int) make8(register_value, 0);
+			crc = CRC16(resp, 6);
+			resp[6] = (int) make8(crc, 1);
+			resp[7] = (int) make8(crc, 0);
+
+			break;
+
+		case WRITE_MULTIPLE_REGISTERS_COMMAND:
+
+			size = (size_t)(8);
+			resp = NULL;
+			resp = (int *) malloc(size);
+
+			if (resp == NULL) {
+				free(resp);
+				return OUT_OF_MEMORY_EXCEPTION;
+			}
+
+			for (cont = 0; cont < b_count; ++cont) {
+				write_eeprom((2 * register_address + cont),
+						buff[cont + BYTE_COUNT + 1]);
+			}
+
+			resp[0] = my_address;
+			resp[1] = WRITE_MULTIPLE_REGISTERS_COMMAND;
+			resp[2] = (int) make8(register_address, 1);
+			resp[3] = (int) make8(register_address, 0);
+			resp[4] = (int) make8(register_value, 1);
+			resp[5] = (int) make8(register_value, 0);
+			crc = CRC16(resp, 6);
+			resp[6] = (int) make8(crc, 1);
+			resp[7] = (int) make8(crc, 0);
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	for (cont = 0; cont < size; ++cont) {
+#ifdef USE_UART1
+#if (SL1_TYPE == SL_MASTER_SLAVE)
+		fputc(resp[cont], sl1);
+#endif
+#endif
+
+#ifdef USE_UART2
+#if (SL2_TYPE == SL_MASTER_SLAVE)
+		fputc(resp[cont], sl2);
+#endif
+#endif
+	}
+
+	free(resp);
+
+	if (door == SL1_DOOR)
+		flush_buffer(buffer_rda, &index_rda);
+	else
+		flush_buffer(buffer_rda2, &index_rda2);
+
+	return NO_EXCEPTION;
 }
 
-uint16_t swap16(uint16_t in) {
-	return make_word(make_byte(in, 0), make_byte(in, 1));
+int modbus_init(int id, int slave_address) {
+
+	my_address = id;
+	slv_address = slave_address;
+
+#ifdef USE_UART1
+	uc_turn_off_timer_rda();
+	uc_clear_rda_interrupt();
+	uc_set_timer_rda();
+	clear_interrupt(INT_RDA);
+	enable_interrupts(INT_RDA);
+	clear_interrupt(RDA_INTERRUPT);
+	enable_interrupts(RDA_INTERRUPT);
+#endif
+
+#ifdef USE_UART2
+	uc_clear_rda2_interrupt();
+	uc_turn_off_timer_rda2();
+	uc_set_timer_rda2();
+	clear_interrupt(INT_RDA2);
+	enable_interrupts(INT_RDA2);
+	clear_interrupt(RDA2_INTERRUPT);
+	enable_interrupts(RDA2_INTERRUPT);
+#endif
+
+	enable_interrupts(GLOBAL);
+
+	return 0;
 }
 
-uint16_t CRC16(uint8_t *nData, uint16_t wLength) {
-	uint8_t nTemp;
-	uint16_t wCRCWord = 0xFFFF;
+int parse_error(modbus_exception_t error, char *msg) {
+	switch (error) {
+	case NO_EXCEPTION:
+		strcpy(msg, "no errors");
+		break;
+	case TIMEOUT_EXCEPTION:
+		strcpy(msg, "time out");
+		break;
+	case OUT_OF_MEMORY_EXCEPTION:
+		strcpy(msg, "out of memory");
+		break;
+	case CRC_EXCEPTION:
+		strcpy(msg, "crc error");
+		break;
+	case WRONG_RESPONSE_EXCEPTION:
+		strcpy(msg, "wrong response");
+		break;
+	case MODBUS_EXCEPTION:
+		strcpy(msg, "modbus exception");
+		break;
+	default:
+		strcpy(msg, "unknown error");
+		break;
+	}
+	return 0;
+}
+
+long CRC16(int *nData, long wLength) {
+	int nTemp;
+	long wCRCWord = 0xFFFF;
 
 	while (wLength--) {
 		nTemp = *nData++ ^ wCRCWord;
@@ -75,106 +288,327 @@ uint16_t CRC16(uint8_t *nData, uint16_t wLength) {
 	return wCRCWord;
 }
 
-uint8_t *make_request(uint8_t dev_addr, uint16_t reg_addr, uint16_t reg_data,
-		uint8_t type) {
+int make_request(int dev_addr, long from, long size, modbus_command_t command,
+		int *req) {
+	long crc;
+
+	req[0] = dev_addr;
+	req[1] = (int) command;
+	req[2] = make8(from, 1);
+	req[3] = make8(from, 0);
+	req[4] = make8(size, 1);
+	req[5] = make8(size, 0);
+	crc = CRC16(req, 6);
+	req[6] = make8(crc, 0);
+	req[7] = make8(crc, 1);
+
+	return 0;
+}
+
+void flush_buffer(int *buffer, long *index) {
+
+	for (erase_cont = 0; erase_cont < *index; ++erase_cont)
+		buffer[erase_cont] = 0;
+
+	*index = 0;
+}
+
+//funcao que leia a resposta do plc
+modbus_exception_t read_plc_response(int *req, int *resp, door_t sl) {
+	long i, time_out, expected_size, cont;
+	modbus_command_t cmd;
+
+	i = 0;
+	time_out = 0;
+	cmd = (modbus_command_t) req[1];
+	expected_size = 0;
+
+	if (sl == SL1_DOOR) {
+#ifdef USE_UART1
+		disable_interrupts(RDA_INTERRUPT);
+		index_rda = 0;
+#endif
+	} else {
+#ifdef USE_UART2
+		disable_interrupts(RDA2_INTERRUPT);
+		index_rda2 = 0;
+#endif
+	}
+
+	switch (cmd) {
+	case READ_HOLDING_REGISTERS_COMMAND:
+		expected_size = (make16(req[4], req[5]) << 1) + 5;
+		break;
+	case READ_COILS_COMMAND:
+	case READ_DISCRETE_INPUT_COMMAND:
+		expected_size = make16(req[4], req[5]);
+		if (expected_size <= 8)
+			expected_size = (expected_size / 8) + 5;
+		else
+			expected_size = (expected_size / 8) + 6;
+		break;
+	}
+
+	if (sl == SL1_DOOR) {
+#ifdef USE_UART1
+		for (i = 0; i < REQUEST_SIZE; ++i) {
+			fputc(req[i], sl1);
+		}
+
+		while (!interrupt_active(RDA_INTERRUPT)) {
+			delay_us(10);
+			time_out++;
+			if (time_out == 0xFFFF) {
+				flush_buffer(buffer_rda, &index_rda);
+				enable_interrupts(RDA_INTERRUPT);
+				return TIMEOUT_EXCEPTION;
+			}
+		}
+
+		uc_clear_rda_interrupt();
+		uc_turn_off_timer_rda();
+		uc_set_timer_rda();
+
+		if (index_rda != 0 && index_rda == expected_size) {
+			for (cont = 0; cont < index_rda; ++cont) {
+				resp[cont] = buffer_rda[cont];
+				buffer_rda[cont] = 0;
+			}
+			index_rda = 0;
+		} else if (index_rda != expected_size) {
+			flush_buffer(buffer_rda, &index_rda);
+			enable_interrupts(RDA_INTERRUPT);
+			return WRONG_RESPONSE_EXCEPTION;
+		}
+		enable_interrupts(RDA_INTERRUPT);
+#endif
+	} else {
+#ifdef USE_UART2
+		for (i = 0; i < REQUEST_SIZE; ++i) {
+			fputc(req[i], sl2);
+		}
+
+		while (!interrupt_active(RDA2_INTERRUPT)) {
+			delay_us(10);
+			time_out++;
+			if (time_out == 0xFFFF) {
+				flush_buffer(buffer_rda2, &index_rda2);
+				enable_interrupts(RDA2_INTERRUPT);
+				return TIMEOUT_EXCEPTION;
+			}
+		}
+
+		uc_clear_rda2_interrupt();
+		uc_turn_off_timer_rda2();
+		uc_set_timer_rda2();
+
+		if (index_rda2 != 0 && index_rda2 == expected_size) {
+			for (cont = 0; cont < index_rda2; ++cont) {
+				resp[cont] = buffer_rda2[cont];
+				buffer_rda2[cont] = 0;
+			}
+			index_rda2 = 0;
+		} else if (index_rda2 != expected_size) {
+			flush_buffer(buffer_rda2, &index_rda2);
+			enable_interrupts(RDA2_INTERRUPT);
+			return WRONG_RESPONSE_EXCEPTION;
+		}
+		enable_interrupts(RDA2_INTERRUPT);
+#endif
+	}
+
+	return NO_EXCEPTION;
+}
+modbus_exception_t mount_modbus_response(modbus_rx_t *modbus_response,
+		int *resp) {
+	int cont, size, *data;
+
+	data = NULL;
+	size = resp[2];
+
+	modbus_response->address = resp[0];
+	modbus_response->function = resp[1];
+	modbus_response->response_size = size;
+
+	free(modbus_response->data);
+	data = (int *) malloc((size_t) size);
+
+	if (data == NULL) {
+		free(data);
+		return OUT_OF_MEMORY_EXCEPTION;
+	}
+
+	for (cont = 0; cont < size; ++cont)
+		data[cont] = resp[cont + 3];
+
+	modbus_response->data = data;
+	modbus_response->crc = make16(resp[size + 4], resp[size + 3]);
+
+	return NO_EXCEPTION;
+}
+
+short check_CRC(int *resp) {
+	int *aux;
+	long size, crc_check, crc_in, cont;
+
+	size = resp[2] + 3;
+	aux = (int *) malloc((size_t) size);
+
+	if (aux == NULL) {
+		free(aux);
+		return FALSE;
+	}
+
+	for (cont = 0; cont < size; ++cont)
+		aux[cont] = resp[cont];
+
+	crc_in = make16(resp[size + 1], resp[size]);
+	crc_check = CRC16(aux, size);
+	free(aux);
+
+	return crc_check == crc_in;;
+}
+
+modbus_exception_t make_transaction(int dev_addr, long from, long size,
+		modbus_command_t command, modbus_rx_t *modbus_response, door_t sl) {
+	int *req, *resp;
+	size_t resp_size;
+	modbus_exception_t resul;
+
+	resul = NO_EXCEPTION;
+	resp_size = 0;
+	req = NULL;
+	req = (int *) malloc((size_t) REQUEST_SIZE);
+
+	if (req == NULL) {
+		free(req);
+		return OUT_OF_MEMORY_EXCEPTION;
+	}
+
+	make_request(dev_addr, from, size, command, req);
+
+	switch (command) {
+	case READ_DISCRETE_INPUT_COMMAND:
+	case READ_COILS_COMMAND:
+		if (size <= 8)
+			resp_size = (size_t)(size / 8 + 5);
+		else
+			resp_size = (size_t)(size / 8 + 6);
+		break;
+	case READ_HOLDING_REGISTERS_COMMAND:
+		resp_size = (size_t)(size << 1) + 5;
+		break;
+	}
+
+	resp = NULL;
+	resp = (int *) malloc(resp_size);
+
+	if (resp == NULL) {
+		free(req);
+		free(resp);
+		return OUT_OF_MEMORY_EXCEPTION;
+	}
+
+	resul = read_plc_response(req, resp, sl);
+	free(req);
+
+	if (resul != NO_EXCEPTION) {
+		free(resp);
+		return resul;
+	}
+
+	if (!check_CRC(resp))
+		return CRC_EXCEPTION;
+
+	resul = mount_modbus_response(modbus_response, resp);
+	free(resp);
+
+	if (resul != NO_EXCEPTION)
+		return resul;
+
+	return NO_EXCEPTION;
+}
+
+int get_word_mem(modbus_rx_t *device, long *resp) {
+	long cont, size_bytes, size_array;
+	int aux_h, aux_l;
+
+	size_bytes = device->response_size;
+	size_array = device->response_size / sizeof(long);
+
+	for (cont = 0; cont < size_array; ++cont) {
+		aux_h = device->data[2 * cont];
+		aux_l = device->data[(2 * cont) + 1];
+		resp[cont] = make16(aux_h, aux_l);
+	}
+
+	return 0;
+}
+
+int get_byte_mem(modbus_rx_t *device, int *resp) {
 	int cont;
-	uint8_t *ret, aux[6];
-	req_un_t request;
 
-	request.structure.addr = dev_addr;
-	request.structure.cmd = type;
-	request.structure.from = swap16(reg_addr);
-	request.structure.to = swap16(reg_data);
+	for (cont = 0; cont < device->response_size; ++cont)
+		resp[cont] = device->data[cont];
 
-	for (cont = 0; cont < 6; ++cont)
-		aux[cont] = request.string[cont];
-
-	request.structure.crc = CRC16(aux, 6);
-	ret = (uint8_t *) malloc(sizeof(REQUEST_SIZE));
-
-	for (cont = 0; cont < REQUEST_SIZE; ++cont)
-		ret[cont] = request.string[cont];
-
-	return ret;
+	return 0;
 }
 
-uint8_t *make_read_request(uint8_t dev_addr, uint16_t from, uint16_t nr) {
-	return make_request(dev_addr, from, nr, READ_HOLDING_REGISTERS);
+modbus_exception_t transport(int dev_addr, long from, long size, long *resp,
+		modbus_command_t command, door_t sl) {
+	modbus_rx_t *modbus_response;
+	modbus_exception_t exception;
+
+	modbus_response = NULL;
+	exception = NO_EXCEPTION;
+	modbus_response = (modbus_rx_t *) malloc(sizeof(modbus_rx_t));
+
+	if (modbus_response == NULL) {
+		free(modbus_response->data);
+		free(modbus_response);
+		return OUT_OF_MEMORY_EXCEPTION;
+	}
+
+	exception = make_transaction(dev_addr, from, size, command, modbus_response,
+			sl);
+
+	if (exception != NO_EXCEPTION) {
+		free(modbus_response->data);
+		free(modbus_response);
+		return exception;
+	}
+
+	switch (command) {
+	case READ_COILS_COMMAND:
+	case READ_DISCRETE_INPUT_COMMAND:
+		get_byte_mem(modbus_response, resp);
+		break;
+	case READ_HOLDING_REGISTERS_COMMAND:
+		get_word_mem(modbus_response, resp);
+		break;
+	}
+
+	free(modbus_response->data);
+	free(modbus_response);
+
+	return NO_EXCEPTION;
+
 }
 
-uint8_t *make_write_request(uint8_t dev_addr, uint16_t reg_addr,
-		uint16_t reg_value) {
-	return make_request(dev_addr, reg_addr, reg_value, WRITE_SINGLE_REGISTER);
+modbus_exception_t read_holding_registers(int dev_addr, long from, long size,
+		long *to, door_t sl) {
+	return transport(dev_addr, from, size, to, READ_HOLDING_REGISTERS_COMMAND,
+			sl);
 }
 
-uint8_t *make_discrete_inputs_request(uint8_t dev_addr, uint16_t reg_addr,
-		uint16_t reg_value) {
-	return make_request(dev_addr, reg_addr, reg_value, READ_DISCRETE_INPUTS);
+modbus_exception_t read_discrete_inputs(int dev_addr, long from, long size,
+		long *to, door_t sl) {
+
+	return transport(dev_addr, from, size, to, READ_DISCRETE_INPUT_COMMAND, sl);
 }
 
-uint8_t *make_read_coils_request(uint8_t dev_addr, uint16_t reg_addr,
-		uint16_t reg_value) {
-	return make_request(dev_addr, reg_addr, reg_value, READ_COILS);
-}
+modbus_exception_t read_coils(int dev_addr, long from, long size, long *to,
+		door_t sl) {
 
-uint8_t *alloc_response(uint8_t *request, int *size) {
-	uint8_t *ret;
-
-	*size = (int) make_word(request[4], request[5]) * 2;
-
-	if (*size <= 0)
-		return NULL;
-
-	*size += 5;
-	ret = (uint8_t *) malloc(*size);
-
-	return ret;
-}
-
-//int get_device(int fd, uint8_t dev_addr, uint16_t reg_addr, uint16_t reg_data,
-//		uint8_t *device_memorie) {
-//	uint8_t *request, *response;
-//	int n, cont, ret;
-//
-//	request = make_read_request(dev_addr, reg_addr, reg_data);
-//	response = (uint8_t *) malloc(sizeof(BUFFER_S));
-//
-//	n = make_transaction(fd, request, response, REQUEST_SIZE, BUFFER_S);
-//
-//	if (n == -1)
-//		return -1;
-//
-//	ret = response[2];
-//
-//	for (cont = 0; cont < ret; ++cont)
-//		device_memorie[cont] = response[cont + 3];
-//
-//	free(request);
-//	free(response);
-//
-//	return ret;
-//}
-
-uint8_t *fromFloat(float value) {
-	uint8_t *ret;
-
-	ret = (uint8_t *) malloc(4);
-	un.a = value;
-
-	ret[1] = un.b[0];
-	ret[0] = un.b[1];
-	ret[3] = un.b[2];
-	ret[2] = un.b[3];
-
-	return ret;
-}
-
-float toFloat(uint8_t *reg) {
-
-	un.b[0] = reg[1];
-	un.b[1] = reg[0];
-	un.b[2] = reg[3];
-	un.b[3] = reg[2];
-
-	return un.a;
+	return transport(dev_addr, from, size, to, READ_COILS_COMMAND, sl);
 }
